e394027a3265a34a3deb84b188503308c4bb0c28
one-data-cookie <kolacek.m@gmail.com>
2024-11-25 01:26:00 +0100

    Play around (finally)

--- playground.py
+++ playground.py
@@ -6,6 +6,12 @@ app = marimo.App(width="medium")
 
 @app.cell
 def __():
+    import marimo as mo
+    return (mo,)
+
+
+@app.cell
+def __():
     import pandas as pd
 
     df = pd.read_csv('AwesomeHabits.csv')
@@ -14,20 +20,46 @@ def __():
 
 
 @app.cell
-def __():
-    import marimo as mo
-    return (mo,)
+def __(df, mo):
+    sql = mo.sql(
+        f"""
+        with
+        prep as (
+            select
+                strptime(Date, '%d %b %Y')::date AS day,
+                Name as name,
+                Quantity::float as quantity
+            from df
+            where Name = 'Sleep'
+        )
+
+        select
+            day,
+            avg(quantity) over (
+                order by day
+                range between interval 30 day preceding and current row    
+            )/60 as moving_avg_30d
+        from prep
+        order by day
+        """
+    )
+    return (sql,)
 
 
 @app.cell
-def __(df, mo):
-    _df = mo.sql(
-        f"""
-        select * 
-        from df
-        limit 10
-        """
-    )
+def __(sql):
+    import matplotlib.pyplot as plt
+
+    plt.plot(sql['day'], sql['moving_avg_30d'])
+    plt.xlabel('Day')
+    plt.ylabel('30d Moving Avg (hours)')
+    plt.title('Average Sleep')
+    plt.gca()
+    return (plt,)
+
+
+@app.cell
+def __():
     return
 
 
